c3c1388b256bf65b8931e529c67cc402fefb8ddb
bertonebreno <bertonebreno@gmail.com>
2022-04-15 19:29:27 -0300

    fix(atividade2): Supressing prints

--- atividade_2_zero_funcoes/secant_iter.m
+++ atividade_2_zero_funcoes/secant_iter.m
@@ -1,11 +1,11 @@
-E = 1e-15
-current_point = 1
-next_point = 2
+E = 1e-15;
+current_point = 1;
+next_point = 2;
 
-point_value = 99999999
-iter = 0
+point_value = 99999999;
+iter = 0;
 while abs(point_value) > E
     iter += 1
-    [current_point, next_point] = next_point_secant(current_point, next_point)
+    [current_point, next_point] = next_point_secant(current_point, next_point);
     point_value = p(next_point)
 endwhile
